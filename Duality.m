805e69d0329c9eb3a3337ab2e1f075c35c6dcce0
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2016-03-05 16:13:57 -0600

    add

--- Duality.m
+++ Duality.m
@@ -8,67 +8,98 @@ A = [H11'*g1*g1'*H11 H11'*g1*g1'*H12 H11'*g1*g1'*H13;H12'*g1*g1'*H11 H12'*g1*g1'
 B = [H21'*g2*g2'*H21 H21'*g2*g2'*H22 H21'*g2*g2'*H23;H22'*g2*g2'*H21 H22'*g2*g2'*H22 H22'*g2*g2'*H23;H23'*g2*g2'*H21 H23'*g2*g2'*H22 H23'*g2*g2'*H23];
 C = [H31'*g3*g3'*H31 H31'*g3*g3'*H32 H31'*g3*g3'*H33;H32'*g3*g3'*H31 H32'*g3*g3'*H32 H32'*g3*g3'*H33;H33'*g3*g3'*H31 H33'*g3*g3'*H32 H33'*g3*g3'*H33];
 
+Aa = [H11'*g1*g1'*H11 H11'*g1*g1'*H12 ;H12'*g1*g1'*H11 H12'*g1*g1'*H12];
 %Dual Problem
+%{
 for o = -10^(4):10^4
-lambda = o*10^(-3);
-g(o+10^(4)+1) = 3 - k*inv([A+B+C+lambda*eye(6) 0*eye(6) 0*eye(6);0*eye(6) A+B+C+lambda*eye(6) 0*eye(6);0*eye(6) 0*eye(6) A+B+C+lambda*eye(6)])*k'-3*lambda*P+n0*(g1'*g1+g2'*g2+g3'*g3);
+lambda1 = o*10^(-3);
+lambda2 = 2;
+lambda3 = 2;
+
+g(o+10^(4)+1) = 3 - k*inv([A+B+C+lambda1*eye(6) 0*eye(6) 0*eye(6);0*eye(6) A+B+C+lambda2*eye(6) 0*eye(6);0*eye(6) 0*eye(6) A+B+C+lambda3*eye(6)])*k'-P*(lambda1+lambda2+lambda3)+n0*(g1'*g1+g2'*g2+g3'*g3);
 end
 o = 1+0.5*10^4:(3/2)*10^4+1;
 plot(o,g(o))
 axis([1 2*10^4+1 -10^1 10^1])
+%}
 
 %Eigenvalues of ABC Matrix
 E = sort((eig([A+B+C 0*eye(6) 0*eye(6);0*eye(6) A+B+C 0*eye(6);0*eye(6) 0*eye(6) A+B+C])));
 %Least Eigenvalues
 E(10);
 
-
-k*[A+B+C 0*eye(6) 0*eye(6);0*eye(6) A+B+C 0*eye(6);0*eye(6) 0*eye(6) A+B+C]*k';
-det([A+B+C 0*eye(6) 0*eye(6);0*eye(6) A+B+C 0*eye(6);0*eye(6) 0*eye(6) A+B+C]);
-
 %Gradients
-G = -P+norm(k)^2*trace(inv(A+B+C+lambda*eye(6))*inv(A+B+C+lambda*eye(6)));
+%G = -P+norm(k)^2*trace(inv(A+B+C+lambda*eye(6))*inv(A+B+C+lambda*eye(6)));
 
 %Search for Optimal Point
-stepsize = 10^(-3);
-lambda = real(-E(10)/2)
-for n = 1:10^(4)
+%stepsize = 1/real(E(18));
+stepsize = 10^(-2);
+lambda1 = real(-E(10)/2);
+lambda2 = real(-E(10)/2);
+lambda3 = real(-E(10)/2);
+
+for n = 1:10^(3)
 %{
 lambda = real(lambda + stepsize*(-P+norm(k)^2*trace(inv(A+B+C+lambda*eye(6))*inv(A+B+C+lambda*eye(6)))))
 real(-P+norm(k)^2*trace(inv(A+B+C+lambda*eye(6))*inv(A+B+C+lambda*eye(6))))
     %}
-lambda = real(lambda + stepsize*(-P+trace(inv(A+B+C+lambda*eye(6))*k(1:6)'*k(1:6)*inv(A+B+C+lambda*eye(6)))))
-real(-P+trace(inv(A+B+C+lambda*eye(6))*k(1:6)'*k(1:6)*inv(A+B+C+lambda*eye(6))))
+gradient1 = real(-P+trace(inv(A+B+C+lambda1*eye(6))*k(1:6)'*k(1:6)*inv(A+B+C+lambda1*eye(6))));
+lambda1 = real(lambda1 + stepsize*gradient1);
+
+gradient2 = real(-P+trace(inv(A+B+C+lambda2*eye(6))*k(7:12)'*k(7:12)*inv(A+B+C+lambda2*eye(6))));
+lambda2 = real(lambda2 + stepsize*gradient2);
+
+gradient3 = real(-P+trace(inv(A+B+C+lambda3*eye(6))*k(13:18)'*k(13:18)*inv(A+B+C+lambda3*eye(6))));
+lambda3 = real(lambda3 + stepsize*gradient3);
+
+
+
+if((norm(gradient1)+norm(gradient2)+norm(gradient3))< 10^(-6)  )
+            %disp('converges!');
+            break;%break the for loop if it's true the condition
 end
 
--E(10)
--E(10)/2
-P11 = Z11*g11+Z12*g21+Z13*g31;
-P12 = Z11*g12+Z12*g22+Z13*g32;
-P13 = Z11*g13+Z12*g23+Z13*g33;
 
-P21 = Z21*g11+Z22*g21+Z23*g31;
-P22 = Z21*g12+Z22*g22+Z23*g32;
-P23 = Z21*g13+Z22*g23+Z23*g33;
 
-P31 = Z31*g11+Z32*g21+Z33*g31;
-P32 = Z31*g12+Z32*g22+Z33*g32;
-P33 = Z31*g13+Z32*g23+Z33*g33;
 
-v11 = R1\P11;
-v12 = R1\P12;
-v13 = R1\P13;
+end
 
-v21 = R2\P21;
-v22 = R2\P22;
-v23 = R2\P23;
+%{
+lambda1 = 10^(-8);
+lambda2 = 10^(-8);
+lambda3 = 10^(-8);
+%}
 
-v31 = R3\P31;
-v32 = R3\P32;
-v33 = R3\P33;
+V1=(k(1:6)*inv(A+B+C+lambda1*eye(6)))';
+V2=(k(7:12)*inv(A+B+C+lambda2*eye(6)))';
+V3=(k(13:18)*inv(A+B+C+lambda3*eye(6)))';
+v11 = V1(1:2);
+v12 = V1(3:4);
+v13 = V1(5:6);
 
+v21 = V2(1:2);
+v22 = V2(3:4);
+v23 = V2(5:6);
+
+v31 = V3(1:2);
+v32 = V3(3:4);
+v33 = V3(5:6);
+
+(norm(v11)^2+norm(v12)^2+norm(v13)^2);
+
+
+
+
+%Duality Gap
+v = [v11;v12;v13;v21;v22;v23;v31;v32;v33];
+g = 3 - k*inv([A+B+C+lambda1*eye(6) 0*eye(6) 0*eye(6);0*eye(6) A+B+C+lambda2*eye(6) 0*eye(6);0*eye(6) 0*eye(6) A+B+C+lambda3*eye(6)])*k'-P*(lambda1+lambda2+lambda3)+norm([g1;g2;g3])^2*n0;
+L = 3-k*v-(k*v)'+v'*[A+B+C+lambda1*eye(6) 0*eye(6) 0*eye(6);0*eye(6) A+B+C+lambda2*eye(6) 0*eye(6);0*eye(6) 0*eye(6) A+B+C+lambda3*eye(6)]*v...
+    -P*(lambda1+lambda2+lambda3)+norm([g1;g2;g3])^2*n0;
+
+
+a=1;
 %Normalize
-
+%{
 M1 = sqrt(norm(v11)^2+norm(v12)^2+norm(v13)^2);
 v11 = v11/M1;
 v12 = v12/M1;
@@ -83,6 +114,7 @@ M3 = sqrt(norm(v31)^2+norm(v32)^2+norm(v33)^2);
 v31 = v31/M3;
 v32 = v32/M3;
 v33 = v33/M3;
+%}
 end
 
     
---
+++ MSE_b.m
@@ -0,0 +1,52 @@
+function [v11, v12, v13, v21, v22, v23, v31, v32, v33] = MSE_b(Z11, Z12, Z13, Z21, Z22, Z23, Z31, Z32, Z33, g1, g2, g3, n0)
+%update filters by MSE criterion
+
+%MSE
+R1 = (Z11*g1)*(Z11*g1)'+(Z12*g2)*(Z12*g2)'+(Z13*g3)*(Z13*g3)'+n0*eye(2);
+R2 = (Z21*g1)*(Z21*g1)'+(Z22*g2)*(Z22*g2)'+(Z23*g3)*(Z23*g3)'+n0*eye(2);
+R3 = (Z31*g1)*(Z31*g1)'+(Z32*g2)*(Z32*g2)'+(Z33*g3)*(Z33*g3)'+n0*eye(2);
+
+P11 = Z11*g1;
+P12 = Z12*g2;
+P13 = Z13*g3;
+
+P21 = Z21*g1;
+P22 = Z22*g2;
+P23 = Z23*g3;
+
+P31 = Z31*g1;
+P32 = Z32*g2;
+P33 = Z33*g3;
+
+v11 = R1\P11;
+v12 = R1\P12;
+v13 = R1\P13;
+
+v21 = R2\P21;
+v22 = R2\P22;
+v23 = R2\P23;
+
+v31 = R3\P31;
+v32 = R3\P32;
+v33 = R3\P33;
+
+%Normalize
+
+M1 = sqrt(norm(v11)^2+norm(v12)^2+norm(v13)^2);
+v11 = v11/M1;
+v12 = v12/M1;
+v13 = v13/M1;
+
+M2 = sqrt(norm(v21)^2+norm(v22)^2+norm(v23)^2);
+v21 = v21/M2;
+v22 = v22/M2;
+v23 = v23/M2;
+
+M3 = sqrt(norm(v31)^2+norm(v32)^2+norm(v33)^2);
+v31 = v31/M3;
+v32 = v32/M3;
+v33 = v33/M3;
+end
+
+    
+    
