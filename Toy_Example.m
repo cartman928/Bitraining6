622e59488e4ef7b60496ecefa078b6a12cffc0fc
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2016-03-08 18:03:14 -0600

    add

--- Toy_Example.m
+++ Toy_Example.m
@@ -1,5 +1,5 @@
 P=1;
-lambda = 5;
+lambda = -5;
 x = 3;
 for n = 1:10^6
 stepsize = 10^(-3);
@@ -8,8 +8,10 @@ stepsize = 10^(-3);
             L_g = 2*x+lambda;
             x = x - stepsize*L_g;
             end
+            L_p = x^2+lambda*(x+P)
             %Dual Update
             D_g = x+P;
-            lambda = lambda + stepsize*D_g       
+            lambda = lambda + stepsize*D_g;
+            L_d = x^2+lambda*(x+P)
 end
 %plot(lambda)
